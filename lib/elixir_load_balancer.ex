a892ce139d490f1c585b73ad820ecbab7a757ef8
thele116 <thele116@morris.umn.edu>
2015-12-08 15:44:45 -0600

    started adding parsing for config files

--- lib/elixir_load_balancer.ex
+++ lib/elixir_load_balancer.ex
@@ -1,2 +1,22 @@
+defmodule node do
+  defstruct hostname: "cthulhu", cpus: -1, ram: -1
+end
+
+
+
 defmodule ElixirLoadBalancer do
+
+defp parseMasterConf(stuff) do
+  IO.puts(stuff)
+end
+
+
+def main() do
+  case File.read("master.conf") do
+    {:ok, body} -> parseMasterConf(body)
+    {:error, reason} -> IO.puts("master.conf read error: " + reason)
+  end
+end
+
+
 end
