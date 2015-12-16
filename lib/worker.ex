90a8222036b57327710525efb5891f4dc0a887fd
Henry Fellows <fello056@morris.umn.edu>
2015-12-16 15:17:03 -0600

    Worked on workers!

--- lib/worker.ex
+++ lib/worker.ex
@@ -1,42 +1,54 @@
 
 defmodule worker do
-  case File.read("master.conf") do
-    {:ok, body} -> parseMasterConf(body)
-  end
-end
-    Code.eval(string)
-    mapper.start()
-    reducer.start()
-  end
-
-  defmodule mapper
-    def initalize()
-      receive do
-        {:confirm?, pid} ->
-          send (pid {:ready, self(), "Default"})
-        {:go, a, b, filename} ->
-          start(a, b, filename)
-      end
+  def intialize(master_pid)
+    case File.read("processing.ex") do
+      {:ok, body} -> Code.eval(body)
+      {:error, reason} -> IO.puts("worker read error: " + reason)
     end
-
-    def start(a, b, filename)
-      IO.puts("The default mapper module hasn't been replaced.")
-    end
-
+    mapper_pid = spawn(mapper)
+    send(mapper_pid, {:confirm?, self()})
+    reducer_pid = spawn(reducer)
+    send(reducer_pid, {:confirm?, self()})
   end
 
-  defmodule reducer
-    def initalize()
-      receive do
-        {:confirm?, pid} ->
-          send (pid {:ready, self(), "Default"})
-          {:go, a, b, filename} ->
-            start(a, b, filename)
-          end
+  def setup_loop(mapper_pid, mapper_ready?, reducer_pid, reducer_ready?, master_pid)
+    if mapper_ready? && reducer_ready? do
+      messaging_loop(mapper_pid, reducer_pid, master_pid)
+    end
+    receive do
+      {:ready, pid, description} ->
+        case description do
+          "Default" ->
+            IO.puts("Apparently, you just put a default module in the file. Congratulations.")
+          desc -> IO.puts(desc)
         end
+        case pid do
+          ^mapper_pid ->
+            IO.puts("Worker " + self() + "'s mapper is ready.")
+            setup_loop(mapper_pid, true, reducer_pid, reducer_ready?, master_pid)
 
-    def start(a, b, filename)
-      IO.puts("The default reducer module hasn't been replaced.")
+          ^reducer_pid ->
+            IO.puts("Worker " + self() + "'s reducer is ready.")
+            setup_loop(mapper_pid, mapper_ready?, reducer_pid, true, master_pid)
+
+          pid ->
+            IO.puts("A process that shouldn't be talking to this worker sent a message. (>_<)")
+            IO.puts("Worker pid: " + self() + " Other pid: " + pid)
+            setup_loop(mapper_pid, reducer_pid, master_pid)
+        end
     end
+
+  end
+
+  def messaging_loop(mapper_pid, reducer_pid, master_pid)
+    receive do
+      {:map, a, b, filename} ->
+        send(mapper_pid {:go, self(), a, b, filename})
+      {:reduce, list_o_work} ->
+        send(reducer_pid {:go, self(), list_o_work})
+      {:result, x} ->
+        send(master_pid, x, self())
+    end
+    messaging_loop(mapper_pid, reducer_pid, master_pid)
   end
 end
